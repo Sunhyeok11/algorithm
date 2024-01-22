139455f2b67b27d0fdc9f3454d386aa8cdb5c00a
Sunhyeok11 <99015371+Sunhyeok11@users.noreply.github.com>
2024-01-22 21:48:16 +0900

    [Gold IV] Title: 오큰수, Time: 1036 ms, Memory: 163380 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,13 @@
+import sys
+n = int(input())
+ans = [0] * n
+A = list(map(int, input().split()))
+myStack = []
+for i in range(n):
+    while myStack and A[myStack[-1]] < A[i]:
+        ans[myStack.pop()] = A[i]   
+    myStack.append(i)
+while myStack:  
+    ans[myStack.pop()] = -1
+for i in range(n):
+    sys.stdout.write(str(ans[i]) + " ")
